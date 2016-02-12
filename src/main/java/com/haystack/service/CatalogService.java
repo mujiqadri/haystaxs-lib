661861642eafd8d4b8cf926f931127705fee4c20
Muji- <mqadri@gopivotal.com>
2016-02-12 02:45:58 -0500

    Added CreateUserSchema in Cluster.java

--- src/main/java/com/haystack/service/CatalogService.java
+++ src/main/java/com/haystack/service/CatalogService.java
@@ -247,7 +247,19 @@ public class CatalogService {
                     log.error("Error in Processing SQL:" + workloadId + ": search_path=" + current_search_path + " SQL=" + nQry + " Exception:" + e.toString());
                 }
             }
+
             rsQry.close();
+
+            // Calculate AST Summary Stats
+            sql = "update tpcds_at_gmail_dot_com.ast \n" +
+                    "set count = X.count, total_duration = X.total_duration\n" +
+                    "FROM \n" +
+                    "\t(select ast_id, count(*) as count , sum(extract(epoch from C.logduration)) as total_duration\n" +
+                    "\tfrom tpcds_at_gmail_dot_com.ast_queries B, tpcds_at_gmail_dot_com.queries C\n" +
+                    "\twhere B.queries_id = C.id \n" +
+                    "\tgroup by ast_id ) X\n" +
+                    "WHERE ast.ast_id = X.ast_id;";
+            dbConnect.execNoResultSet(sql);
 
             ms.scoreModel();
             ms.generateRecommendations();
